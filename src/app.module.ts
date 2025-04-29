2ce3295dc8109e1caf456ff36bd5ce71602cce80
ggalvaod <duartegabrielgalvao@gmail.com>
2025-04-29 20:51:33 -0300

    atualizado_29-04

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Evento } from './evento/evento.entity';
 import { EventoModule } from './evento/evento.module';
+import { RateLimitMiddleware } from './evento/rate-limit.middleware';
+
 @Module({
 imports: [
 TypeOrmModule.forRoot({
@@ -17,4 +19,10 @@ synchronize: true, // em produção, mude para false
 EventoModule,
 ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+      consumer
+        .apply(RateLimitMiddleware)
+        .forRoutes('*'); // aplica o middleware para todas as rotas
+    }
+  }
